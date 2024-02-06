c08676f164086de1d935aca2bc3de657a54cec54
DevNasim01 <sknasimpc@gmail.com>
2024-02-06 13:25:58 +0530

    2ND DAY

--- script.js
+++ script.js
@@ -28,7 +28,21 @@ page_1_bt.addEventListener("mouseleave", handleMouseLeave);
 
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('main'),
-    smooth: true
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    repeat: true,
+    multiplier: 0.6
 });
+
+// let btn_text_box = document.querySelectorAll(".btn_text_box");
+// let i = document.querySelectorAll('.btn_text_box-i');
+
+// function con(){
+//     i.style.left = '74%';
+// }
+
+// for (let b of btn_text_box) {
+//     b.addEventListener("mouseover", con);
+// }
+
 
